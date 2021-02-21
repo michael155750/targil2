9c0314d9536ceb1744d58d418d8ceb1ccf38a8d1
מיכאל <michael155750@gmail.com>
2021-02-21 20:00:42 +0200

    Add project files.

---
+++ targil2/func.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+
+/**
+ * @file funcs.h
+ */
---
+++ targil2/main.c
@@ -0,0 +1,11 @@
+#include "func.h"
+
+/**
+ * @brief
+ * @return 0 for successfull finish
+ * assumptions:
+*/
+int main()
+{
+    return 0;
+}
